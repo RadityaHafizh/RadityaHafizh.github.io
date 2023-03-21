eab01eb43dce4dcb022b971fc20c128bcadf9166
Raditya Hafizh <raditya.habibi17@gmail.com>
2023-03-21 21:06:11 +0700

    initial commit

---
+++ Eksternal.js
@@ -0,0 +1,21 @@
+alert("Selamat Datang di Website Raditya");
+
+function home() {
+  alert("Selamat Datang di Home");
+}
+
+function biodata() {
+  alert("Selamat Datang di Biodata");
+}
+
+function gallery() {
+  alert("Selamat Datang di Galeri");
+}
+
+function form() {
+  alert("Selamat Datang di Forms");
+}
+
+function Submit() {
+  alert("Terkirim");
+}
